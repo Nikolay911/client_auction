181f718c596ed88b932add78ad359cf1bef896c0
Nikolay911 <81089615+Nikolay911@users.noreply.github.com>
2021-08-11 13:18:24 +0300

    Update WebSecurityConfig.java

--- java/com/example/auction1_client/security/WebSecurityConfig.java
+++ java/com/example/auction1_client/security/WebSecurityConfig.java
@@ -1,15 +1,11 @@
 package com.example.auction1_client.security;
 
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 
-@Configuration
-@EnableConfigurationProperties
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
